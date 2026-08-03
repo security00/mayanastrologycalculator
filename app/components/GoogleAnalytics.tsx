cc6eb8df62badde5f94cb19314a563cbfe1a1312
potter <xiangqiling5204@gmail.com>
2026-08-03 23:04:17 +0800

    feat: add Google Ads global tag

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,21 +1,25 @@
 import Script from 'next/script';
 
+const GOOGLE_ANALYTICS_ID = 'G-4R36DG2336';
+const GOOGLE_ADS_ID = 'AW-17631642548';
+
 export default function GoogleAnalytics() {
   return (
     <>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-4R36DG2336"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`}
         strategy="afterInteractive"
       />
       <Script
-        id="google-analytics"
+        id="google-tag"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-4R36DG2336');
+            gtag('config', '${GOOGLE_ANALYTICS_ID}');
+            gtag('config', '${GOOGLE_ADS_ID}');
           `,
         }}
       />
